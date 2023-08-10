91e87f2b618836049c72f3a380f8cbea4b831c74
hikari_7776 <24840795+Hikari1nVoid@users.noreply.github.com>
2023-08-10 11:13:18 +0800

    Update main.java

--- main.java
+++ main.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
-
+import java.lang.String;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.logging.log4j.Level;
 public class Crackmethod{
